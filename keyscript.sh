5d2b4691a20e113de0047242437f965d8c777890
artosan <artosank@gmail.com>
2017-12-03 16:08:41 +0200

    Disable echo when entering PIN

--- keyscript.sh
+++ keyscript.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo "*** Trying to unlock the dick $CRYPTTAB_SOURCE ($CRYPTTAB_NAME) by nitrokey, waiting for the USB to be recognized..." >&2
 
+# disable echo
+stty -echo
+
 # sleep to wait for the usb device to be available
 # TODO is there better way to do this?
 sleep 5
